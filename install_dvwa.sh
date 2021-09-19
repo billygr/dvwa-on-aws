6098dc04c3295e950c1787ac22315f8ca66a3a62
billygr <billygr@users.noreply.github.com>
2021-09-19 14:06:16 +0300

    DVWA settings

--- install_dvwa.sh
+++ install_dvwa.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 apt update
 
-# Install prerewcuites for DVWA 
-#apt-get -y install apache2 mariadb-server php php-mysqli php-gd libapache2-mod-php
+# Install prerequisites for DVWA 
+apt-get -y install apache2 mariadb-server php php-mysqli php-gd libapache2-mod-php
 
 # Database
+sudo mysqladmin create dvwa
 
+sudo mysql dvwa << EOF
+create user dvwa@localhost identified by 'p@ssw0rd';
+grant all on dvwa.* to dvwa@localhost;
+flush privileges;
+EOF
