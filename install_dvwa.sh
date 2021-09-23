6dcd8365381e193c05ef0b45d0cee6a28f29be2c
billygr <billygr@users.noreply.github.com>
2021-09-23 08:14:58 +0300

    Changes for php on dvwa

--- install_dvwa.sh
+++ install_dvwa.sh
@@ -20,3 +20,7 @@ sudo cp config/config.inc.php.dist config/config.inc.php
 sudo chown www-data:www-data /var/www/html/hackable/uploads/
 sudo chown www-data:www-data /var/www/html/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
 sudo chown www-data:www-data /var/www/html/config
+
+# PHP changes for dvwa
+sudo sed -i 's/allow_url_include = Off/allow_url_include = On/g' /etc/php/7.2/apache2/php.ini
+sudo apache2ctl restart
