3f9992ef12e839bc5fa4fea3449f534d3e607963
Andre Fernandes <andre.fernandes86@gmail.com>
2023-04-10 12:06:14 -0500

    Update get_ami_image.tf

--- get_ami_image.tf
+++ get_ami_image.tf
@@ -3,7 +3,7 @@ This file is dedicated to finding the right Ubuntu AMI to install on  our server
 AMI IDs change from region to region and change over time as  upgrades come out. 
 We're going to create a data source to track down the right one.
 A data source is a read-only view into data stored  outside of Terraform. 
-In this case, we are creating an aws_ami data source with the unique identifier of ubuntu.
+In this case, we are creating an aws_ami data source with the unique identifier of ubuntu..
  */
 data "aws_ami" "ubuntu" {
   most_recent = true
